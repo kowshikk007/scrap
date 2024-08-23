49fe6d58ac3ef00bdad8dbf8a792af2c4195500c
kowshikk007 <kowshikk786@gmail.com>
2024-08-23 17:58:41 +0530

    Update and rename python.py to app.py

--- python.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request
-import pickle
-import numpy as np
-app = Flask(__name__)
-@app.route('/')
-def hello():
-    """test functions"""
-    return "Welcome to PVP college"
-app.run()
